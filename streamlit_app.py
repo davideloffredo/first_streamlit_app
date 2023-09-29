265c7641560fc15d2d2ecc1022768fa547e76248
davideloffredo <davide.loffredo@we-plus.eu>
2023-09-29 15:16:26 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,6 +21,13 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 streamlit.dataframe(fruits_to_show)
 
+# New Section
 streamlit.header('Fruity Fruit Advice')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+streamlit.text(fruityvice_normalized)
+
+
+
