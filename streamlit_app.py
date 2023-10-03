9cb87a24ecfc9648336ef35614bb77c114854c29
Dave <davide.loffredo@we-plus.eu>
2023-10-03 12:10:41 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 import requests
 import snowflake.connector
-import urllib.error import error
+from urllib.error import error
 
 streamlit.header('Breakfast favorites')
 
