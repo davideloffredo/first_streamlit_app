0ae6afa4bb9d9fcad93ce2dbc287a1bf8bbdea70
Dave <davide.loffredo@we-plus.eu>
2023-10-03 10:54:22 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 import pandas
 import requests
-#import snowflake.connector
+
 
 
 streamlit.header('Breakfast favorites')
@@ -38,5 +38,5 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
 
-
+import snowflake.connector
 
